37f70911c491bb944fe69a721fe642611978a6e8
sujin9335 <sujin9335@gmail.com>
2024-08-30 14:20:48 +0900

    vue 버전

---
+++ vue-front/src/components/header.vue
@@ -0,0 +1,85 @@
+
+<template lang="">
+    <div class="container">
+        <header
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            <a href="/home" class="d-inline-flex link-body-emphasis text-decoration-none">
+                home
+            </a>
+        </div>
+        
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" id="menu">
+            <li><a href="/board"  id="menu_board" 
+                :class="[
+                    { 'active': $route.path === '/board' },
+                    {'nav-link px-2': true}
+                ]
+                ">게시판</a></li>
+            <li><a href="/user" id="menu_user" :class="[
+                    { 'active': $route.path === '/user' },
+                    {'nav-link px-2': true}
+                ]
+                ">유저관리</a></li>
+            <li><a href="/group" id="menu_group" :class="[
+                    { 'active': $route.path === '/group' },
+                    {'nav-link px-2': true}
+                ]
+                ">그룹관리</a></li>
+            <!-- <li><a href="/test" id="menu_group" :class="[
+                    { 'active': $route.path === '/test' },
+                    {'nav-link px-2': true}
+                ]
+                ">Test</a></li> -->
+            <!-- <li><a href="/test2" id="menu_group" :class="[
+                    { 'active': $route.path === '/test2' },
+                    {'nav-link px-2': true}
+                ]
+                ">Test2</a></li> -->
+            <li><a href="/test3" id="menu_group" :class="[
+                    { 'active': $route.path === '/test3' },
+                    {'nav-link px-2': true}
+                ]
+                ">구성</a></li>
+        </ul>
+        
+
+        
+        <!-- 로그아웃 버튼 표시 -->
+        <div class="col-md-3 text-end">
+            
+
+            <span>안녕하세요 {{ userName }}님</span>
+            <button type="button" class="btn btn-outline-primary me-2" @click="authStore.logout()">Logout</button>
+            <!-- <button type="button" class="btn btn-outline-primary me-2"
+            onclick="location.href='/'">Login</button> -->
+        </div>
+        
+
+        
+        </header>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { useAuthStore } from "@/stores/auth";
+
+export default defineComponent({
+    data() {
+        return {
+            authStore: useAuthStore(),
+        };
+    },
+    computed: {
+        userName() {
+            return this.authStore.loginInfo?.user_name || "게스트";
+        },
+    },
+});
+</script>
+<style scoped>
+.active {
+    color: white;
+    background-color: green;
+}
+</style>
---
+++ vue-front/src/components/modals/contents.vue
@@ -0,0 +1,109 @@
+<template>
+    <!-- <div v-if="isVisible" @click.self="closeModal"> -->
+    <div >
+        <div class="modal show" id="composition" data-bs-backdrop="static" data-bs-keyboard="true"
+         aria-labelledby="staticBackdropLabel" aria-hidden="true" style="display: block;">
+            <div class="modal-dialog modal-dialog-centered modal-custom">
+                <div class="modal-content">
+
+                    <div class="modal-body">
+                        <div class="container" id="main">
+                            <div class="row">
+                                <div class=" text-end">
+                                        <button id="norBtn" type="button" class="btn btn-success" @click="closeModal">닫기</button>
+                                </div>
+                                <div class="col-5 me-5 border hi-700" id="menu">
+                                    <div class="my-2">구성 리스트</div>
+                                    <div class="border hi-500 p-2 scroll" >
+                                        <table class="table text-center" id="userTab">
+                                            <thead>
+                                                <tr>
+                                                    <th class="col-2" scope="col">
+                                                        <input class="form-check-input" type="checkbox" value=""
+                                                            id="checkboxAll">
+                                                    </th>
+                                                    <th class="col-5" scope="col">타이틀</th>
+                                                    <th class="col-5" scope="col">콘텐츠</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="text-end m-2">
+                                        <button>삭제</button>
+                                    </div>
+                                </div>
+                                <div class="col border hi-700" id="content">
+                                    <!-- 메뉴 -->
+                                    
+                                    <!-- 내용 -->
+                                    <div class="border m-2">
+                                        <!-- <div class="container" style="display: none;"> -->
+                                        <div class="container text-center" id="normalCon">
+                                            <div class="row row-cols-2">
+                                                <div class="col-2 my-2">콘텐츠</div>
+                                                <div class="col-10 my-2">
+                                                    <select >
+                                                        <!-- <option v-for="">
+
+                                                        </option> -->
+                                                    </select>
+                                                </div>
+                                                <div class="col-2">타이틀</div>
+                                                <div class="col-10 mb-2">
+                                                    <input class="form-control" type="text" id="text">
+                                                </div>
+                                                <div class="col-2">데이터</div>
+                                                <div class="col-10">
+                                                    <textarea class="form-control" rows="17" id="info"></textarea>
+                                                    <input type="hidden" id="id">
+                                                </div>
+                                            </div>
+                                            <div class="text-end m-2">
+                                                <button >등록</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+
+export default {
+    // props: { //부모에서 넘어온것
+    //     isVisible: {
+    //         type: Boolean, //타입설명
+    //         required: true, //필수 인지
+    //     },
+    // },
+    setup(props, { emit }) {
+
+        const monitorData = [
+            {
+
+            }
+        ];
+
+
+        const closeModal = () => {
+            emit('close');
+        };
+
+        return {
+            closeModal,
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
